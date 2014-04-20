0bc182b6ec568513d9c8dda1a91cffb1cf2ed51d
Craig Martin <craig.j.martin2@gmail.com>
2014-04-19 18:57:29 -0700

    Updated Parse with new app keys

--- app/src/main/java/org/onewarmcoat/onewarmcoat/app/MainActivity.java
+++ app/src/main/java/org/onewarmcoat/onewarmcoat/app/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         //all API initialization should be done in some function
-        Parse.initialize(this, "59auZqm4QfvdrnqRKD0OsT9ZwTehkmlqLEexQ3wL", "q6tM18L2hX7hkaw7BNxZGywdwSqguC1qcfPLNw71");
+        Parse.initialize(this, "c8IKIZkRcbkiMkDqdxkM4fKrBymrX7p7glVQ6u8d", "EFY5RxFnVEKzNOMKGKa3JqLR6zJlS4P6z0OPF3Mt");
 
         //TODO: create a setupViews function to wrap all of this
         donateFragment = new DonateFragment();
