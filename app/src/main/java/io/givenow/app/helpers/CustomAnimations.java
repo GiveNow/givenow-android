dfd3a9599de760e9544fa69f653d1b512014b398
Afik Cohen <aphex@aphex.cx>
2015-11-26 01:56:18 +0100

    Fix crash related to ViewAnimationUtils being API>=21 only; add a fallback fading animation for older apis.

--- app/src/main/java/io/givenow/app/helpers/CustomAnimations.java
+++ app/src/main/java/io/givenow/app/helpers/CustomAnimations.java
@@ -1,6 +1,7 @@
 package io.givenow.app.helpers;
 
 import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
 
+import io.givenow.app.R;
 import io.givenow.app.interfaces.AnimatorStartListener;
 
 public class CustomAnimations {
@@ -63,11 +65,28 @@ public class CustomAnimations {
         // get the final radius for the clipping circle
         int finalRadius = Math.max(v.getWidth(), v.getHeight());
 
-        // create the animator for this view (the start radius is zero)
-        Animator anim = ViewAnimationUtils.createCircularReveal(v, cx, cy, 0, finalRadius);
+        Animator anim;
+        if (android.os.Build.VERSION.SDK_INT >= 21) { //ViewAnimationUtils is only API >= 21
+            // create the animator for this view (the start radius is zero)
+            anim = ViewAnimationUtils.createCircularReveal(v, cx, cy, 0, finalRadius);
+        } else { //Or fall back to a simple fade in
+            anim = AnimatorInflater.loadAnimator(v.getContext(), R.animator.fade_in);
+        }
 
         // make the view visible and start the animation
-        anim.addListener((AnimatorStartListener) animation -> v.setVisibility(View.VISIBLE));
+//        anim.addListener((AnimatorStartListener) animation -> v.setVisibility(View.VISIBLE));
+        //the above line is a culprit in the following exception:
+//        11-26 01:37:19.330 7938-7938/io.givenow.app A/art: art/runtime/thread.cc:1329] Throwing new exception 'length=3413; index=4199' with unexpected pending exception: java.lang.ArrayIndexOutOfBoundsException: length=3413; index=4199
+//        11-26 01:37:19.331 7938-7938/io.givenow.app A/art: art/runtime/thread.cc:1329]   at io.givenow.app.interfaces.AnimatorStartListener io.givenow.app.helpers.CustomAnimations$$Lambda$5.lambdaFactory$(android.view.View) ((null):-1)
+//        11-26 01:37:19.331 7938-7938/io.givenow.app A/art: art/runtime/thread.cc:1329]   at android.animation.Animator io.givenow.app.helpers.CustomAnimations.circularReveal(android.view.View) (CustomAnimations.java:77)
+        //so let's use an AnimatorListenerAdapter instead!
+        anim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                v.setVisibility(View.VISIBLE);
+            }
+        });
         return anim;
     }
 
@@ -82,8 +101,12 @@ public class CustomAnimations {
         int initialRadius = v.getWidth();
 
         // create the animation (the final radius is zero)
-        Animator anim =
-                ViewAnimationUtils.createCircularReveal(v, cx, cy, initialRadius, 0);
+        Animator anim;
+        if (android.os.Build.VERSION.SDK_INT >= 21) { //ViewAnimationUtils is only API >= 21
+            anim = ViewAnimationUtils.createCircularReveal(v, cx, cy, initialRadius, 0);
+        } else { //Or fall back to a simple fade out
+            anim = AnimatorInflater.loadAnimator(v.getContext(), R.animator.fade_out);
+        }
 
         // make the view invisible when the animation is done
         anim.addListener(new AnimatorListenerAdapter() {
