6b12a0a83e2d6c8efc4abf5583f7f11f50eb2c39
Afik Cohen <aphex@aphex.cx>
2015-12-02 17:03:08 +0100

    Enable Revocable sessions in Parse, which could come in handy in the future.

--- app/src/main/java/io/givenow/app/GiveNowApplication.java
+++ app/src/main/java/io/givenow/app/GiveNowApplication.java
@@ -69,6 +69,7 @@ public class GiveNowApplication extends Application {
     private void initializeParse() {
         Parse.initialize(this, "c8IKIZkRcbkiMkDqdxkM4fKrBymrX7p7glVQ6u8d", "EFY5RxFnVEKzNOMKGKa3JqLR6zJlS4P6z0OPF3Mt");
         ParseUser.enableAutomaticUser();
+        ParseUser.enableRevocableSessionInBackground();
         ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 
